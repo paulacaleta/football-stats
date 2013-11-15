ce6338bd9210a8f3caf8c6443dec94cdc1f392ed
easlavov@gmail.com <easlavov@gmail.com@d15695b6-cfa9-8149-fafd-c55c1c8014dd>
2013-11-15 10:17:55 +0000

    Added Clubs folder. Added two interfaces in Clubs - IClub.cs and IClubStats.cs

---
+++ trunk/FootballStats/FootballStats/Clubs/IClub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FootballStats.Common;
+using FootballStats.Persons;
+
+namespace FootballStats.Clubs
+{
+    public interface IClub
+    {
+        string Name { get; }
+        Nationality Nationality { get; }
+        string Location { get; }        
+        List<Player> Team { get; }
+        List<StaffMember> Staff { get; }
+
+        void ChangeName(string newName);
+        void ChangeNationality(Nationality newNationality);
+        void ChangeLocation(string newLocation);
+        void AddPlayer(Player player);
+        void RemovePlayer(Player player);
+        void AddStaffMember(StaffMember staffMember);
+        void RemoveStaffMember(StaffMember staffMember);
+    }
+}
---
+++ trunk/FootballStats/FootballStats/Clubs/IClubStats.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballStats.Clubs
+{
+    public interface IClubStats
+    {
+        double TeamAverageAge();
+        int TotalPlayersAtClub();
+        int TotalGoalkeepers();
+        int TotalDefenders();
+        int TotalForwards();
+        bool HasManager();
+        decimal AverageWage();
+        decimal HighestWage();
+    }
+}
