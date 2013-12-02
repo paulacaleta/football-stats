773c6bec461b35aaa4eed92e5e4f5e521a0b8596
bmcheezer@gmail.com <bmcheezer@gmail.com@d15695b6-cfa9-8149-fafd-c55c1c8014dd>
2013-12-02 01:35:38 +0000

    Finally made the ExtendedToString() into a generic method, and we don't have to repeat the same code 6 times!!!!

--- trunk/FootballStats/FootballStats/Common/ToStringExtentionsForCollections.cs
+++ trunk/FootballStats/FootballStats/Common/ToStringExtentionsForCollections.cs
@@ -8,7 +8,7 @@
 
     public static class ToStringExtentionsForCollections
     {
-        public static string ExtendedToString(this List<Player> list)
+        public static string ExtendedToString<T>(this List<T> list)
         {
             StringBuilder sb = new StringBuilder();
 
@@ -16,76 +16,6 @@
             foreach (var player in list)
             {
                 sb.AppendLine(player.ToString());
-                sb.Append('\n');
-            }
-
-            return sb.ToString();
-        }
-
-        public static string ExtendedToString(this List<StaffMember> list)
-        {
-            StringBuilder sb = new StringBuilder();
-
-            sb.AppendLine(new string('-', 80));
-            foreach (var staffMember in list)
-            {
-                sb.AppendLine(staffMember.ToString());
-                sb.Append('\n');
-            }
-
-            return sb.ToString();
-        }
-
-        public static string ExtendedToString(this List<MatchEvent> list)
-        {
-            StringBuilder sb = new StringBuilder();
-
-            sb.AppendLine(new string('-', 80));
-            foreach (var events in list)
-            {
-                sb.AppendLine(events.ToString());
-                sb.Append('\n');
-            }
-
-            return sb.ToString();
-        }
-
-        public static string ExtendedToString(this List<Match> list)
-        {
-            StringBuilder sb = new StringBuilder();
-
-            sb.AppendLine(new string('-', 80));
-            foreach (var match in list)
-            {
-                sb.AppendLine(match.ToString());
-                sb.Append('\n');
-            }
-
-            return sb.ToString();
-        }
-
-        public static string ExtendedToString(this List<Club> list)
-        {
-            StringBuilder sb = new StringBuilder();
-
-            sb.AppendLine(new string('-', 80));
-            foreach (var club in list)
-            {
-                sb.AppendLine(club.ToString());
-                sb.Append('\n');
-            }
-
-            return sb.ToString();
-        }
-
-        public static string ExtendedToString(this List<Referee> list)
-        {
-            StringBuilder sb = new StringBuilder();
-
-            sb.AppendLine(new string('-', 80));
-            foreach (var referee in list)
-            {
-                sb.AppendLine(referee.ToString());
                 sb.Append('\n');
             }
 
