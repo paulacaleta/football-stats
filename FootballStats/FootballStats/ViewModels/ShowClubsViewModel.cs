0c8ea8e37faf0f11f7d96ef395252f225a04ef3c
easlavov <easlavov@gmail.com>
2013-12-04 16:15:43 +0000

    GUI: Added ShowClubsWindow and ViewModels/ShowClubsViewModel.cs - yet to be finished

---
+++ FootballStats/FootballStats/ViewModels/ShowClubsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FootballStats.Clubs;
+using FootballStats.Competitions;
+
+namespace FootballStats.ViewModels
+{
+    public class ShowClubsViewModel
+    {
+        public IEnumerable<Club> Clubs
+        {
+            get
+            {
+                World.Load();
+                return World.Clubs;
+            }
+        }
+    }
+}
