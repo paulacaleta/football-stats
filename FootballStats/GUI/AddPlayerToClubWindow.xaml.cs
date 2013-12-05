9306347bce46304fc069eb62ee10b2c90e7efc31
easlavov <easlavov@gmail.com>
2013-12-05 11:36:55 +0000

    GUI: Added new windows. Further improved functionality.

---
+++ FootballStats/GUI/AddPlayerToClubWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using FootballStats.Clubs;
+using FootballStats.Persons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GUI
+{
+    /// <summary>
+    /// Interaction logic for AddPlayerToClubWindow.xaml
+    /// </summary>
+    public partial class AddPlayerToClubWindow : Window
+    {
+        private Club club;
+        public AddPlayerToClubWindow(Club club)
+        {
+            InitializeComponent();
+            this.club = club;
+            this.ClubTextBlock.Text = this.club.ToString();
+        }
+
+        private void OnAddButtonClick(object sender, RoutedEventArgs e)
+        {
+            club.AddPlayer(this.PlayersListBox.SelectedItem as Player);
+            (this.PlayersListBox.SelectedItem as Player).AfiliatedClub = club.Name;
+        }
+    }
+}
